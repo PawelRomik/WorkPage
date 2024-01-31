6eddd78f7f88b1a7331837e8d680b99e972dff83
Paweł Romik <leoneczek15@gmail.com>
2024-01-31 20:18:02 +0100

    Fixed Settings

---
+++ src/components/Settings/SettingsPasswordSection/SettingsPasswordSection.tsx
@@ -0,0 +1,72 @@
+import { useMemo } from "react";
+import "./SettingsPasswordSection.style.scss";
+
+type SettingsPasswordSectionProps = {
+	password: string;
+	oldPassword: string;
+	newPassword: string;
+	unlock: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	changePass: () => void;
+	unsetPass: () => void;
+	handleOldPasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	handleNewPasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SettingsPasswordSection = ({
+	password,
+	oldPassword,
+	newPassword,
+	unlock,
+	changePass,
+	unsetPass,
+	handleNewPasswordChange,
+	handleOldPasswordChange,
+}: SettingsPasswordSectionProps) => {
+	const sectionTitle = useMemo(() => (password ? "CHANGE YOUR PASSWORD" : "SET YOUR PASSWORD"), [password]);
+	return (
+		<section className='changePasswordSection'>
+			<h2>{sectionTitle}</h2>
+			<div className='passContainer'>
+				{password && (
+					<input
+						className='changePasswordInput'
+						value={oldPassword}
+						onChange={handleOldPasswordChange}
+						type='password'
+						id='oldpassword'
+						name='oldpassword'
+						placeholder='Old password'
+					/>
+				)}
+				<input
+					className='changePasswordInput'
+					value={newPassword}
+					onChange={handleNewPasswordChange}
+					type='password'
+					id='confirmPassword'
+					name='confirmPassword'
+					placeholder='New Password'
+				/>
+
+				<div className='changePasswordButtons'>
+					{password ? (
+						<>
+							<button className='unlockButton' onClick={changePass}>
+								Change
+							</button>
+							<button className='removeButton' onClick={unsetPass}>
+								Remove password
+							</button>
+						</>
+					) : (
+						<button className='unlockButton' onClick={unlock}>
+							Confirm
+						</button>
+					)}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SettingsPasswordSection;
---
+++ src/components/Settings/SettingsWallpaperSection/SettingsWallpaperSection.tsx
@@ -0,0 +1,55 @@
+import wallpaperData from "../../../data/wallpapers";
+import { useMemo } from "react";
+import "./SettingsWallpaperSection.style.scss";
+
+type SettingsWallpaperSectionProps = {
+	changeBackground: (e: React.MouseEvent) => void;
+	backgroundInputValue: string;
+	handleInputKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+	handleCustomWallpaper: () => void;
+	handleBackgroundInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SettingsWallpaperSection = ({
+	changeBackground,
+	backgroundInputValue,
+	handleInputKeyDown,
+	handleCustomWallpaper,
+	handleBackgroundInputChange,
+}: SettingsWallpaperSectionProps) => {
+	const wallpapers = useMemo(
+		() =>
+			wallpaperData.map((el, i) => (
+				<button key={i} className='wallpaperButton' onClick={changeBackground}>
+					<img src={el} alt='wallpaper' />
+				</button>
+			)),
+		[changeBackground]
+	);
+
+	return (
+		<section className='changeWallpaperSection'>
+			<h2>SET YOUR WALLPAPER</h2>
+
+			<div className='wallpaperPanel'>
+				<input
+					className='wallpaperInput'
+					type='text'
+					name='wallpaperInput'
+					value={backgroundInputValue}
+					onChange={handleBackgroundInputChange}
+					id='wallpaperInput'
+					placeholder={`Custom wallpaper: (url)`}
+					onKeyDown={handleInputKeyDown}
+				></input>
+				<button className='wallpaperConfirmButton' onClick={handleCustomWallpaper}>
+					<i className='fa-solid fa-arrow-right-to-bracket'></i>
+				</button>
+			</div>
+
+			<div className='wallpapersSelection'>{wallpapers}</div>
+		</section>
+	);
+};
+
+export default SettingsWallpaperSection;
