10bdcd0b79d5ccebf34cdbdbf32ac6e5ea6febb1
Paweł Romik <leoneczek15@gmail.com>
2024-03-12 20:20:45 +0100

    Changed login styles

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,23 +1,25 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Clock from "../../components/Clock/Clock";
 import "./Login.style.scss";
 import { useSettingsContext } from "../../providers/SettingsContext";
 import { useAuthContext } from "../../providers/AuthContext";
 import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation";
 import LoginProfile from "../../components/LoginProfile/LoginProfile";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { css } from "@emotion/react";
 
 const Login = () => {
 	const { login } = useAuthContext();
-	const { password } = useSettingsContext();
+	const { password, darkMode, background, wallpaperStyle } = useSettingsContext();
 	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState<undefined | string>(undefined);
 	const [loginInput, setLoginInput] = useState("");
 
 	const handleLogin = useCallback(() => {
 		if (password === loginInput) {
 			setLoading(true);
 		} else {
-			setError("Incorrect password");
+			toast.error("Incorrect password");
 		}
 	}, [loginInput, password]);
 
@@ -35,15 +37,38 @@ const Login = () => {
 		[handleLogin]
 	);
 
+	const backgroundStyles = useMemo(
+		() => css`
+			&.loginScreen::before {
+				background-image: url(${background});
+				background-size: ${wallpaperStyle};
+			}
+		`,
+		[]
+	);
+
 	return (
-		<main className='loginScreen'>
+		<main className='loginScreen' css={backgroundStyles}>
 			<div className='loginTimer'>
 				<Clock />
 			</div>
+			<ToastContainer
+				position='top-right'
+				limit={2}
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover={false}
+				theme={darkMode ? "light" : "dark"}
+			/>
 			{loading ? (
 				<LoadingAnimation animationEnd={animationEnd} repeats={2} />
 			) : (
-				<LoginProfile handleKeyPress={handleKeyPress} handleLogin={handleLogin} error={error} password={password} loginInput={loginInput} updateLoginInput={setLoginInput} />
+				<LoginProfile handleKeyPress={handleKeyPress} handleLogin={handleLogin} password={password} loginInput={loginInput} updateLoginInput={setLoginInput} />
 			)}
 		</main>
 	);
