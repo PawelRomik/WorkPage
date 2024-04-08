1244d7fd9e47e8022820ca6a2889dfce1ee76f8f
Paweł Romik <leoneczek15@gmail.com>
2024-04-08 15:47:48 +0200

    Video History: Added confirm modal

--- src/components/VideoPlayer.tsx/VideoHistory/VideoHistory.tsx
+++ src/components/VideoPlayer.tsx/VideoHistory/VideoHistory.tsx
@@ -2,7 +2,9 @@ import { useTranslation } from "react-i18next";
 import { useSettingsContext } from "../../../providers/SettingsContext";
 import type { Video } from "../VideoPlayer";
 import { videoHistorySectionStyles } from "./VideoHistory.styles";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 type VideoHistoryProps = {
 	videoData: Video[];
@@ -13,13 +15,38 @@ const VideoHistory = ({ videoData, changeCurrentVideoUrl }: VideoHistoryProps) =
 	const { darkMode, color } = useSettingsContext();
 	const { t } = useTranslation();
 
+	const showModal = useCallback(
+		(index: number) => {
+			withReactContent(Swal)
+				.fire({
+					title: t("Swal.swalTitle"),
+					text: t("Player.videoChangeModal"),
+					showCancelButton: true,
+					confirmButtonColor: darkMode ? "#dfdfdf" : "rgb(27, 27, 27)",
+					cancelButtonColor: darkMode ? "#dfdfdf" : "rgb(27, 27, 27)",
+					confirmButtonText: t("Swal.swalYes"),
+					cancelButtonText: t("Swal.swalNo"),
+					background: darkMode ? "white" : "black",
+					color: darkMode ? "black" : "white",
+					showCloseButton: true,
+					target: ".notesContainer",
+				})
+				.then((result) => {
+					if (result.isConfirmed) {
+						changeCurrentVideoUrl(videoData[index].videoUrl);
+					}
+				});
+		},
+		[changeCurrentVideoUrl, darkMode, t, videoData]
+	);
+
 	const videoHistoryElements = useMemo(
 		() =>
 			Array.from({ length: 8 }, (_, index) => {
 				if (index < videoData.length) {
 					return (
 						<div className='videoHistoryItem hasVideo' key={index}>
-							<img src={videoData[index].thumbnail} title={t("Player.videoHistoryHoverText")} onClick={() => changeCurrentVideoUrl(videoData[index].videoUrl)} />
+							<img src={videoData[index].thumbnail} title={t("Player.videoHistoryHoverText")} onClick={() => showModal(index)} />
 						</div>
 					);
 				} else {
@@ -33,7 +60,7 @@ const VideoHistory = ({ videoData, changeCurrentVideoUrl }: VideoHistoryProps) =
 					);
 				}
 			}),
-		[changeCurrentVideoUrl, videoData, t]
+		[videoData, t, showModal]
 	);
 
 	return (
