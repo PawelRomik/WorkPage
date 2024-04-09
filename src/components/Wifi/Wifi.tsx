31993dfd36cabb3504f4e1d4676d007e5ffa78e0
Paweł Romik <leoneczek15@gmail.com>
2024-04-09 22:21:48 +0200

    Wifi: now displays user's ip

--- src/components/Wifi/Wifi.tsx
+++ src/components/Wifi/Wifi.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSettingsContext } from "../../providers/SettingsContext";
 import { useTranslation } from "react-i18next";
 import { wifiContainerStyles } from "./Wifi.styles";
@@ -6,14 +6,46 @@ import { wifiContainerStyles } from "./Wifi.styles";
 const Wifi = () => {
 	const { color, darkMode } = useSettingsContext();
 	const { t } = useTranslation();
+	const [ip, changeApi] = useState("");
+	const [ipVisibility, changeIpVisibility] = useState(false);
 
 	const dontHideOnClick = useCallback((e: React.MouseEvent) => {
 		e.stopPropagation();
 	}, []);
 
+	useEffect(() => {
+		const fetchIp = async () => {
+			try {
+				const locationResponse = await fetch(import.meta.env.VITE_GEOLOCATION_API_URL);
+				if (!locationResponse.ok) {
+					throw new Error("Failed to fetch location");
+				}
+
+				const locationResult = await locationResponse.json();
+				changeApi(locationResult.ip);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchIp();
+	}, []);
+
 	return (
 		<div className='wifiContainer' css={wifiContainerStyles(darkMode, color)} onClick={dontHideOnClick}>
-			<p>
+			<div className='ipContainer'>
+				<p>IP:</p>
+				<p className='ipParagraph'>
+					{!ipVisibility && (
+						<div className='hideIpBlock' onClick={() => changeIpVisibility(true)}>
+							{t("Wifi.showIp")}
+						</div>
+					)}
+					<div onClick={() => changeIpVisibility(false)} title={t("Wifi.hideIp")}>
+						{ip}
+					</div>
+				</p>
+			</div>
+			<p className='wifiConnected'>
 				<i className='fa-solid fa-wifi'></i>
 				{t("Wifi.wifiConnected")}
 			</p>
