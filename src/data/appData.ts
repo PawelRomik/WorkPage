f6ca81a8a9998df860b99484536b33344b195d20
Paweł Romik <leoneczek15@gmail.com>
2025-06-27 10:18:38 +0200

    fixes

--- src/data/appData.ts
+++ src/data/appData.ts
@@ -2,41 +2,36 @@ export const appData = [
 	{
 		id: 0,
 		name: "Settings",
-		class: "fa-solid fa-gear",
+		class: "fa-solid fa-gear"
 	},
 	{
 		id: 1,
 		name: "Notes",
-		class: "fa-regular fa-note-sticky",
+		class: "fa-regular fa-note-sticky"
 	},
 	{
 		id: 2,
 		name: "ToDoList",
-		class: "fa-solid fa-list-check",
+		class: "fa-solid fa-list-check"
 	},
 	{
 		id: 3,
 		name: "Calculator",
-		class: "fa-solid fa-calculator",
+		class: "fa-solid fa-calculator"
 	},
 	{
 		id: 4,
 		name: "Paint",
-		class: "fa-solid fa-palette",
+		class: "fa-solid fa-palette"
 	},
 	{
 		id: 5,
 		name: "Minesweeper",
-		class: "fa-solid fa-land-mine-on",
+		class: "fa-solid fa-land-mine-on"
 	},
 	{
 		id: 6,
-		name: "Translator",
-		class: "fa-solid fa-language",
-	},
-	{
-		id: 7,
 		name: "Player",
-		class: "fa-solid fa-circle-play",
-	},
+		class: "fa-solid fa-circle-play"
+	}
 ];
